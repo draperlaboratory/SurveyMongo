e8e5a72f4964f27a6eadb3fda5fe50f2ffd275c8
skintigh <skintigh@gmail.com>
2015-07-17 19:59:54 -0400

    many updates
    name optional, wildcard default, info to stderr, fixed to output to
    screen only when launched from command line

--- get_data/get_survey_details.py
+++ get_data/get_survey_details.py
@@ -1,22 +1,31 @@
 # get survey details
 
+from __future__ import print_function
 import sys
 import requests
 import json
 from sm_functions import get_survey_list, get_respondent_list, get_responses, get_survey_details
-import time
-import pprint
 
-def run(name):
+
+def run(name=""):
     with open("private_key.txt", "r") as f:
         client = requests.session()       
         client.headers = json.loads(f.readline())
         client.params = json.loads(f.readline())
         survey_name = str(name)
+        print ("Info: Downloading survey list", file=sys.stderr)
         survey_list = get_survey_list(client, survey_name)
 
+        ret = []
         for survey in reversed(survey_list['data']['surveys']):
-            json.dump(get_survey_details(client, survey['survey_id']), sys.stdout)
+            print ("Info: Downloading", survey["title"], file=sys.stderr)
+            ret.append(get_survey_details(client, survey['survey_id']))
+        return ret
         
 if __name__ == '__main__':
-    run(sys.argv[1])    
+    if len(sys.argv) > 1:
+        ret = run(sys.argv[1])
+    else:
+        ret = run()
+    for item in ret:
+        json.dump(item, sys.stdout)
