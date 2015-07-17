448382fd04ff5ae3f5c979d3e7cceea537ea7c0c
skintigh <skintigh@gmail.com>
2015-07-17 18:32:31 -0400

    get survey names
    linux script to get all survey names

---
+++ get_data/get_survey_names.sh
@@ -0,0 +1,1 @@
+curl -H 'Authorization:bearer paTBz61kMlD0mPWrsaHR3921EuYbHlBvqU0GDZQ.5nahBvB1GbdZpbh2WnOzXY4S1m4CvPfPmXO9cABCSafzRjNeH9zln7ik1mnK.DPSQSI=' -H 'Content-Type: application/json' https://api.surveymonkey.net/v2/surveys/get_survey_list/?api_key=xda4n5zxycnbhxqy84pb6bas --data-binary '{"fields":["title"]}' > surveys.json
