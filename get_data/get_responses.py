ccc1a532af2d03931955a73cb6b68390e9219e0d
skintigh <skintigh@gmail.com>
2015-07-17 19:53:45 -0400

    name optional, wildcard default, stderr info

--- get_data/get_responses.py
+++ get_data/get_responses.py
@@ -1,34 +1,38 @@
 # get the responses to the survey
 
+from __future__ import print_function
 import sys
 import requests
 import json
 from sm_functions import get_survey_list, get_respondent_list, get_responses, get_survey_details
-import time
-import pprint
 
-def run(name):
+
+def run(name=""):
     with open("private_key.txt", "r") as f:
         client = requests.session()
         client.headers = json.loads(f.readline())
         client.params = json.loads(f.readline())
 
         survey_name = str(name)
+        print ("Info: Downloading survey list", file=sys.stderr)
         survey_list = get_survey_list(client, survey_name)
 
         respondent_list = {}
 
         ret = []
         for survey in reversed(survey_list['data']['surveys']):
+            print ("Info: Downloading", survey["title"], file=sys.stderr)
             respondent_list[survey['survey_id']] = get_respondent_list(client, survey['survey_id'])
             
             for respondent in respondent_list[survey['survey_id']]['data']['respondents']:
                 ret.append(get_responses(client, survey['survey_id'], [respondent['respondent_id']]))
-                #json.dump(get_responses(client, survey['survey_id'], [respondent['respondent_id']]), sys.stdout)
               
         return ret
         
 if __name__ == '__main__':
-    ret = run(sys.argv[1])   
+    if len(sys.argv) > 1:
+        ret = run(sys.argv[1])
+    else:
+        ret = run() 
     for item in ret:
         json.dump(item, sys.stdout)
