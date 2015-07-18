9c0fdb9a7e590b4fa1644217c9d3d5904a6f4d41
skintigh <skintigh@gmail.com>
2015-07-17 20:52:40 -0400

    name optional, wildcard default, added info stderr, fixed print

--- get_data/get_survey_ids.py
+++ get_data/get_survey_ids.py
@@ -1,25 +1,29 @@
-# get a list of surveys 
+# get a list of survey ids 
+
+from __future__ import print_function
 import sys
 import requests
 import json
 from sm_functions import get_survey_list, get_respondent_list, get_responses, get_survey_details
-import time
 import pprint
 
-def run(name):
+def run(name=""):
     with open("private_key.txt", "r") as f:
         client = requests.session()       
         client.headers = json.loads(f.readline())
         client.params = json.loads(f.readline())
-
         survey_name = str(name)
+        print ("Info: Downloading survey list", file=sys.stderr)
         survey_list = get_survey_list(client, survey_name)
+        return survey_list
 
-        if sys.version_info[0] > 2:
-            #print(survey_list) # only works correctly in python 3
-            pprint.pprint(survey_list) # only works correctly in python 3
-        else: 
-            json.dumps(survey_list, indent=2, sort_keys=True) #only works in python 2
     
 if __name__ == '__main__':
-    run(sys.argv[1])    
+    if len(sys.argv) > 1:
+        ret = run(sys.argv[1])
+    else:
+        ret = run()
+    if sys.version_info[0] > 2:
+        pprint.pprint(ret) # only works correctly in python 3
+    else: 
+        print (json.dumps(ret, indent=2, sort_keys=True)) #only works in python 2    
