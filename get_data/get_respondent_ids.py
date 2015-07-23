c69306b03a39df42406d7f1b70f4af751c5778bd
skintigh <skintigh@gmail.com>
2015-07-22 23:17:06 -0400

    changed to optparse, added date field, cleaned imports

--- get_data/get_respondent_ids.py
+++ get_data/get_respondent_ids.py
@@ -4,9 +4,10 @@ from __future__ import print_function
 import sys
 import requests
 import json
-from sm_functions import get_survey_list, get_respondent_list, get_responses, get_survey_details
+from sm_functions import get_survey_list, get_respondent_list
+import argparse
 
-def run(name=""):
+def run(name="", date=None):
     with open("private_key.txt", "r") as f:
         client = requests.session()       
         client.headers = json.loads(f.readline())
@@ -18,13 +19,16 @@ def run(name=""):
         ret = []
         for survey in reversed(survey_list['data']['surveys']):
             print ("Info: Downloading respondent IDs for", survey["title"], file=sys.stderr)
-            ret.append(get_respondent_list(client, survey['survey_id']))
+            ret.append(get_respondent_list(client, survey['survey_id'], date))
         return ret
     
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        ret = run(sys.argv[1])
-    else:
-        ret = run()
+    parser = argparse.ArgumentParser(description='Download latest respondent IDs')
+    parser.add_argument('--name', default="", dest='name',
+                   help='Name of survey, default is all')
+    parser.add_argument('--date', default=None, dest='date',
+                   help='Date and time of last read, default is None')
+    args = parser.parse_args()
+    ret = run(args.name, args.date)
     for item in ret:
         json.dump(item, sys.stdout)
