bb6f9e7f2eb1b6f4f9a188423ed8be09f756ff31
skintigh <skintigh@gmail.com>
2015-07-23 22:35:27 -0400

    download only since last date, switched to use argparse, cleaned imports

--- get_data/get_responses.py
+++ get_data/get_responses.py
@@ -4,10 +4,10 @@ from __future__ import print_function
 import sys
 import requests
 import json
-from sm_functions import get_survey_list, get_respondent_list, get_responses, get_survey_details
+from sm_functions import get_survey_list, get_respondent_list, get_responses
 
 
-def run(name=""):
+def run(name="", date=None):
     with open("private_key.txt", "r") as f:
         client = requests.session()
         client.headers = json.loads(f.readline())
@@ -15,24 +15,28 @@ def run(name=""):
 
         survey_name = str(name)
         print ("Info: Downloading survey list", file=sys.stderr)
-        survey_list = get_survey_list(client, survey_name)
+        survey_list = get_survey_list(client, survey_name) #optimize 
 
         respondent_list = {}
 
         ret = []
         for survey in reversed(survey_list['data']['surveys']):
             print ("Info: Downloading responses for", survey["title"], file=sys.stderr)
-            respondent_list[survey['survey_id']] = get_respondent_list(client, survey['survey_id'])
+            respondent_list[survey['survey_id']] = get_respondent_list(client, survey['survey_id'], date) #optimize
             
             for respondent in respondent_list[survey['survey_id']]['data']['respondents']:
-                ret.append(get_responses(client, survey['survey_id'], [respondent['respondent_id']]))
+                ret.append(get_responses(client, survey['survey_id'], [respondent['respondent_id']], date))
               
         return ret
         
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        ret = run(sys.argv[1])
-    else:
-        ret = run() 
+    parser = argparse.ArgumentParser(description='Download latest responses')
+    parser.add_argument('--name', default="", dest='name',
+                   help='Name of survey, default is all')
+    parser.add_argument('--date', default=None, dest='date',
+                   help='Date and time of last read, default is None')
+    args = parser.parse_args()
+    
+    ret = run(args.name, args.date)
     for item in ret:
         json.dump(item, sys.stdout)
--- get_data/get_survey_details.py
+++ get_data/get_survey_details.py
@@ -4,28 +4,32 @@ from __future__ import print_function
 import sys
 import requests
 import json
-from sm_functions import get_survey_list, get_respondent_list, get_responses, get_survey_details
+from sm_functions import get_survey_list, get_survey_details
 
 
-def run(name=""):
+def run(name="", date=None):
     with open("private_key.txt", "r") as f:
         client = requests.session()       
         client.headers = json.loads(f.readline())
         client.params = json.loads(f.readline())
         survey_name = str(name)
         print ("Info: Downloading survey list", file=sys.stderr)
-        survey_list = get_survey_list(client, survey_name)
+        survey_list = get_survey_list(client, survey_name) #optimize
 
         ret = []
         for survey in reversed(survey_list['data']['surveys']):
             print ("Info: Downloading survey details for", survey["title"], file=sys.stderr)
-            ret.append(get_survey_details(client, survey['survey_id']))
+            ret.append(get_survey_details(client, survey['survey_id'], date))
         return ret
         
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        ret = run(sys.argv[1])
-    else:
-        ret = run()
+    parser = argparse.ArgumentParser(description='Download latest survey details')
+    parser.add_argument('--name', default="", dest='name',
+                   help='Name of survey, default is all')
+    parser.add_argument('--date', default=None, dest='date',
+                   help='Date and time of last read, default is None')
+    args = parser.parse_args()
+    
+    ret = run(args.name, args.date)
     for item in ret:
         json.dump(item, sys.stdout)
