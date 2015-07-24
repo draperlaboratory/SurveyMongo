e37a2fac64a923cbf27cf7fe93c51f55980c38a2
skintigh <skintigh@gmail.com>
2015-07-23 22:33:48 -0400

    count downloads
    also attempted to optimize survey list, doesn't seem to help

--- get_data/get_respondent_ids.py
+++ get_data/get_respondent_ids.py
@@ -15,11 +15,14 @@ def run(name="", date=None):
         
         survey_name = str(name)
         print ("Info: Downloading survey list", file=sys.stderr)
-        survey_list = get_survey_list(client, survey_name)
+        survey_list = get_survey_list(client, survey_name, date) #optimize, date does nothing?
         ret = []
+        count = 0
         for survey in reversed(survey_list['data']['surveys']):
             print ("Info: Downloading respondent IDs for", survey["title"], file=sys.stderr)
             ret.append(get_respondent_list(client, survey['survey_id'], date))
+            count += 1
+        print ("Info: %d surveys processed"%count, file=sys.stderr)
         return ret
     
 if __name__ == '__main__':
@@ -29,6 +32,7 @@ if __name__ == '__main__':
     parser.add_argument('--date', default=None, dest='date',
                    help='Date and time of last read, default is None')
     args = parser.parse_args()
+    
     ret = run(args.name, args.date)
     for item in ret:
         json.dump(item, sys.stdout)
