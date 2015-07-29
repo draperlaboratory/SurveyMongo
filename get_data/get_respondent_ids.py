befb756edd890c09a1b25ad2da51308315ea9a8f
skintigh <skintigh@gmail.com>
2015-07-29 12:59:14 -0400

    comment about mystery error

--- get_data/get_respondent_ids.py
+++ get_data/get_respondent_ids.py
@@ -20,6 +20,28 @@ def run(name="", date=None):
         count = 0
         for survey in reversed(survey_list['data']['surveys']):
             print ("Info: Downloading respondent IDs for", survey["title"], file=sys.stderr)
+            #error happened in here once
+            '''
+            Traceback (most recent call last):
+  File "download_all.py", line 18, in <module>
+    ret = get_respondent_ids.run(name="", date=date)
+  File "C:\Users\Seth\Documents\GitHub\SurveyMongo\get_data\get_respondent_ids.p
+y", line 23, in run
+    ret.append(get_respondent_list(client, survey['survey_id'], date))
+  File "C:\Users\Seth\Documents\GitHub\SurveyMongo\get_data\sm_functions.py", li
+ne 48, in get_respondent_list
+    respondents_response_json = respondents_response.json()
+  File "C:\Users\Seth\Anaconda\lib\site-packages\requests\models.py", line 776,
+in json
+    return json.loads(self.text, **kwargs)
+  File "C:\Users\Seth\Anaconda\lib\json\__init__.py", line 338, in loads
+    return _default_decoder.decode(s)
+  File "C:\Users\Seth\Anaconda\lib\json\decoder.py", line 366, in decode
+    obj, end = self.raw_decode(s, idx=_w(s, 0).end())
+  File "C:\Users\Seth\Anaconda\lib\json\decoder.py", line 384, in raw_decode
+    raise ValueError("No JSON object could be decoded")
+ValueError: No JSON object could be decoded
+            '''
             ret.append(get_respondent_list(client, survey['survey_id'], date))
             count += 1
         print ("Info: %d surveys processed"%count, file=sys.stderr)
