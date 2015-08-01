f1a292aed0e57141022a01a79cd344b17aad7e81
skintigh <skintigh@gmail.com>
2015-07-31 23:22:57 -0400

    updates tables instead of dropping them
    untested because SM is down...

--- get_data/get_data.bat
+++ get_data/get_data.bat
@@ -1,13 +1,12 @@
 set mongopath=C:\Progra~1\MongoDB\Server\3.0\bin
 
 @echo Downloading latest data...
-python download_all.py
+c:\Python27\python.exe download_all.py
 
-@echo Drop existing table and insert that data into the collections
-@echo Data does not have uniqe _id fields so updating tables is impossible
-%mongopath%\mongoimport.exe respondent_id_list.json /drop
-%mongopath%\mongoimport.exe responses.json /drop
-%mongopath%\mongoimport.exe survey_details.json /drop
+@echo Insert that data into the collections
+%mongopath%\mongoimport.exe respondent_id_list.json
+%mongopath%\mongoimport.exe responses.json
+%mongopath%\mongoimport.exe survey_details.json
 REM %mongopath%\mongoimport.exe survey_ids.json /drop
 
 echo updating the session_table from those collections
