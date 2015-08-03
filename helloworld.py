7e8cbc9a2f757eab7afaba6b73258fd243fd06a0
Dan Traviglia <dtraviglia@gmail.com>
2015-08-03 14:04:31 -0400

    add helloworld flask test

---
+++ helloworld.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask import request
+from surveymongo_test import collect_responses
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+	user_agent = request.headers.get('User-Agent')
+	return '<h1>Hello World!</h1><p>Your browser is %s</p>' % user_agent
+
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+
+@app.route('/surveymongo/<session_id>')
+def surveymongo(session_id):
+	# this = [{'ID':'one', 'Name':'Bob', 'Role':'developer'}, {'ID':'two'}]
+	responses = collect_responses(session_id)
+	sorted_responses = sorted(responses, key=lambda k: k['question_id'])
+	return render_template('list.html', list=responses)
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
